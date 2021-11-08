7e6bfacada85987591922a3a0aabd2a76a796731
Henrique Rodrigues <henrique.c.rodrigues@hotmail.com>
2021-11-07 21:34:41 -0300

    fix(routes): fix header options in CORS

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
     );
 
     if (req.method === 'OPTIONS') {
-        res.hader('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).send({});
     }
 
